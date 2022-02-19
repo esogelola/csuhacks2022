fa958bc5ad006edd3cd6a80f682fb320e8d93c46
allenchazhoor <allenchazhoor@gmail.com>
2022-02-19 17:27:46 -0500

    Add journal model

---
+++ backend/models/journal.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const journalSchema = new mongoose.Schema({
+
+    dayDescription:{
+        type: String,
+        required: true,
+        max:254
+    },
+    productivityRange:{
+        type: Number
+    },
+    hasExcercised:{
+        type:boolean
+    }
+})
+
+module.exports = mongoose.model("Journal", journalSchema);
