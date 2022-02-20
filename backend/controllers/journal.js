39f3907745fff29a854b48f2046ea1c2ef2b0a75
esogelola <esogelola@gmail.com>
2022-02-20 03:04:21 -0500

    Emotion Analysis comlete

--- backend/controllers/journal.js
+++ backend/controllers/journal.js
@@ -1,89 +1,105 @@
 // Import User model here
+const { default: axios } = require("axios");
 const { Journal } = require("../models");
 
-exports.getAll = async (req, res)=>{
-  
-    const userId = req.body.userId;
-    
-    try
-    {
-        if (userId) {
-            const journal = await Journal.find({userId: userId});
-      
-            res.status(200).json({
-             error: null,
-             journal: journal
-            });
-          } else {
-            res.status(400).json({ error: "userId must be defined" });
-          }
-        
-    }
-    catch(error)
-    {
-        res.status(400).json({error});
-    }
-
-}
-
-
-exports.getOne = async (req, res)=>{
-  
-    const journalId = req.body._id;
-    
-    try
-    {
-        if (journalId) {
-            const journal = await Journal.find({_id: journalId});
-      
-            res.status(200).json({
-             error: null,
-             journal: journal
-            });
-          } else {
-            res.status(400).json({ error: "journal ID must be defined" });
-          }
-        
-    }
-    catch(error)
-    {
-        res.status(400).json({error});
-    }
-
-}
-
-
-
-exports.create = async (req, res)=>{
-
-    const journal = new Journal({
-        userId: req.body.userId,
-        dayDescription: req.body.dayDescription,
-        productivityRange: req.body.productivityRange,
-        hasExercised: req.body.hasExercised
-      });
-    
-      //5 - Save new user to mongodb and return unique ID
-      try {
-        const savedJournal = await journal.save();
-        res.json({ error: null, data: { journal: savedJournal } });
-      } catch (error) {
-        res.status(400).json({ error });
-      }
-
-}
-
-
-exports.status = (req, res) => {
-    res.json({
-      message: "OK",
-      timestamp: new Date().toISOString(),
-      IP: req.ip,
-      URL: req.originalUrl,
-    });
+const getEmotionAnalysis = async (text) => {
+  console.log(text);
+  var options = {
+    method: "GET",
+    url: "https://twinword-emotion-analysis-v1.p.rapidapi.com/analyze/",
+    params: { text: text },
+    headers: {
+      "x-rapidapi-host": process.env.x_rapidapi_host,
+      "x-rapidapi-key": process.env.x_rapidapi_key,
+    },
   };
 
-  
+  return axios
+    .request(options)
+    .then(function (response) {
+      return response.data;
+    })
+    .catch(function (error) {
+      return error;
+    });
+};
+exports.getAll = async (req, res) => {
+  const userId = req.body.userId;
 
+  try {
+    if (userId) {
+      const journal = await Journal.find({ userId: userId });
 
+      res.status(200).json({
+        error: null,
+        journal: journal,
+      });
+    } else {
+      res.status(400).json({ error: "userId must be defined" });
+    }
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+};
 
+exports.getOne = async (req, res) => {
+  const journalId = req.body._id;
+
+  try {
+    if (journalId) {
+      const journal = await Journal.find({ _id: journalId });
+
+      res.status(200).json({
+        error: null,
+        journal: journal,
+      });
+    } else {
+      res.status(400).json({ error: "journal ID must be defined" });
+    }
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+};
+
+exports.create = async (req, res) => {
+  const journalText = req.body.dayDescription;
+
+  if (journalText) {
+    let emotionAnalysis;
+    await getEmotionAnalysis(journalText).then((value) => {
+      emotionAnalysis = value;
+    });
+    console.log(emotionAnalysis.emotion_scores);
+
+    const newJournal = new Journal({
+      userId: req.body.userId,
+      dayDescription: journalText,
+      emotionAnalysis: {
+        emotions_detected: emotionAnalysis.emotions_detected,
+        emotion_scores: emotionAnalysis.emotion_scores,
+        result_code: emotionAnalysis.result_code,
+        result_msg: emotionAnalysis.result_msg,
+      },
+      productivityRange: req.body.productivityRange,
+      hasExercised: req.body.hasExercised,
+    });
+    try {
+      const savedJournal = await newJournal.save();
+
+      res.json({ error: null, data: { journal: savedJournal } });
+    } catch (error) {
+      res.status(400).json({ error: `${error} - Could not save` });
+    }
+  } else {
+    res.status(400).json({ error: "No journal text" });
+  }
+};
+
+exports.status = (req, res) => {
+  res.json({
+    message: "OK",
+    timestamp: new Date().toISOString(),
+    IP: req.ip,
+    URL: req.originalUrl,
+  });
+};
--- backend/models/journal.js
+++ backend/models/journal.js
@@ -1,23 +1,42 @@
 const { number } = require("joi");
 const mongoose = require("mongoose");
 
+const emotionalScores = new mongoose.Schema({
+  fear: Number,
+  anger: Number,
+  sadness: Number,
+  joy: Number,
+  surprise: Number,
+  disgust: Number,
+});
+const emotionAnalysis = new mongoose.Schema({
+  emotions_detected: [String],
+  emotion_scores: emotionalScores,
+  result_code: Number,
+  result_msg: String,
+});
 const journalSchema = new mongoose.Schema({
-    
-    userId:{
-        type: Number,
-        required: true,
-    },
-    dayDescription:{
-        type: String,
-        required: true,
-        max:254
-    },
-    productivityRange:{
-        type: Number
-    },
-    hasExercised:{
-        type:Boolean
-    }
-})
+  userId: {
+    type: Number,
+    required: true,
+  },
+
+  dayDescription: {
+    type: String,
+    required: true,
+    max: 254,
+  },
+  emotionAnalysis: emotionAnalysis,
+  productivityRange: {
+    type: Number,
+  },
+  hasExercised: {
+    type: Boolean,
+  },
+  date: {
+    type: Date,
+    default: Date.now(),
+  },
+});
 
 module.exports = mongoose.model("Journal", journalSchema);
